cc46b0197bbb10867ab4180d495e6ca418e9f4f3
benameryoussef <benameryoussef956@gmail.com>
2023-01-11 04:03:12 -0800

    first commit

---
+++ src/main/java/org/sid/ebankanigbackend/EbankanigbackendApplication.java
@@ -0,0 +1,13 @@
+package org.sid.ebankanigbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankanigbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankanigbackendApplication.class, args);
+    }
+
+}
